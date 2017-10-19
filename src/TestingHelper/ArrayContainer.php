4954229968a46675bf6edd7caeb69ef461f451d1
Daniel Bannert <d.bannert@anolilab.de>
2017-10-19 21:03:42 +0200

    removed support for container-interop/container-interop

--- src/TestingHelper/ArrayContainer.php
+++ src/TestingHelper/ArrayContainer.php
@@ -2,7 +2,7 @@
 declare(strict_types=1);
 namespace Narrowspark\TestingHelper;
 
-use Interop\Container\ContainerInterface;
+use Psr\Container\ContainerInterface;
 
 class ArrayContainer implements ContainerInterface
 {
